c0dceb3b4b40e7a9ee9baa6fb6c081b1fd19c375
Vishal Patil <vishal.patil@intellectdesign.com>
2022-06-04 20:38:27 +0530

    Added flyway scripts for url_metadata table

---
+++ src/main/resources/db/migration/V1__create_urlmetadata.sql
@@ -0,0 +1,11 @@
+CREATE SEQUENCE urlmetadata_id_seq INCREMENT BY 50;
+
+CREATE TABLE urlmetadata(
+  id 									            BIGINT 	      PRIMARY KEY,
+	hash							              VARCHAR(255)	UNIQUE NOT NULL,
+  long_url                        VARCHAR(500)  NOT NULL,
+  short_url                       VARCHAR(50)   NOT NULL,
+  expiration_time                 TIMESTAMP NOT NULL,
+  creation_timestamp              TIMESTAMP,
+  updation_timestamp              TIMESTAMP
+);
